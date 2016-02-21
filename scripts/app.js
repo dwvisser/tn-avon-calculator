1191266d82c65b555a0611500a855a393ff2bc3f
Dale Visser <dale.visser@live.com>
2016-02-21 16:27:20 -0500

    Basic functionality in place. Using jQuery Mobile.

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dale on 2/21/16.
+ */
+
+function getAmount() {
+    var pre_tax = parseFloat($('#pre-tax-total').val())
+    var tn_tax = 0.0975 * pre_tax
+    $('#tn-tax-amount').text(tn_tax.toFixed(2))
+    var surcharge = 0.75
+    $('#avon-surcharge-amount').text(surcharge.toFixed(2))
+    $('#total-amount').text((pre_tax+tn_tax+surcharge).toFixed(2))
+}
+
+$('#pre-tax-total').change(getAmount)
